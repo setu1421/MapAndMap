a3cca06ac87255bce1e1709b68eeac9441e17c38
setu <setu.1421@yahoo.com>
2014-12-25 23:51:09 +0600

    Successfully hide the softkeyboard.

--- app/src/main/java/com/example/mapandmap/mapandmap/HomeFragment.java
+++ app/src/main/java/com/example/mapandmap/mapandmap/HomeFragment.java
@@ -2,11 +2,20 @@ package com.example.mapandmap.mapandmap;
 
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -15,12 +24,16 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
 
-public class HomeFragment extends Fragment {
+
+public class HomeFragment extends Fragment implements View.OnClickListener{
     // ...
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
     static final LatLng KIEL = new LatLng(53.551, 9.993);
     private GoogleMap map;
+    EditText et;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -46,8 +59,57 @@ public class HomeFragment extends Fragment {
         // Zoom in, animating the camera.
         map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 
-
-
+         et = (EditText) v.findViewById(R.id.editText1);
+       //added the go button listener
+       Button go = (Button) v.findViewById(R.id.go);
+        go.setOnClickListener(this);
         return v;
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.go:
+                try {
+                    geoLocate(v);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+
+    private void gotoLocation(double lat, double lng, float zoom) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
+        map.moveCamera(update);
+    }
+
+    public void geoLocate(View v) throws IOException {
+        hideSoftKeyboard(v);
+
+
+        String location = et.getText().toString();
+
+        Geocoder gc = new Geocoder(getActivity());
+        List<Address> list = gc.getFromLocationName(location, 1);
+        Address add = list.get(0);
+        String locality = add.getLocality();
+        Toast.makeText(getActivity(), locality, Toast.LENGTH_LONG).show();
+
+        double lat = add.getLatitude();
+        double lng = add.getLongitude();
+
+        gotoLocation(lat, lng, 15);
+
+    }
+  // to hide keyboard must use getActivity() and Context
+
+    private void hideSoftKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+
 }
